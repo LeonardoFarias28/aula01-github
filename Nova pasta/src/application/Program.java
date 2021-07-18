c1719911b831c33d228d40f72ed72a530277b3e2
Leonardo Farias <34768673+LeonardoFarias28@users.noreply.github.com>
2021-07-18 01:03:18 -0300

    Add files via upload

---
+++ Nova pasta/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		for(int i =0; i<10; i++) {
+			System.out.println(i);
+		}
+		System.out.println("Olá Mundo!");
+	}
+	
+}
